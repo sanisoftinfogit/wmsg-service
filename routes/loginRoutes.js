4a0cbc8fcbad6c9ab4daafa16f65116720d58a3c
Aniket Kumbhar <a20170667747@gmail.com>
2025-09-09 10:43:09 +0530

    error

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { handleLogin } = require('../controllers/loginController');
 
+// POST /login
 router.post('/login', handleLogin);
 
 module.exports = router;
